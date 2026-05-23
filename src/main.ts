40934728f9a86061ce8cd024fe815f4b35098166
Fox <kostya.moroz.96@gmail.com>
2026-05-23 11:11:41 +0300

    fixes

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,36 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        transform: true,
-        forbidNonWhitelisted: true,
-      }),
-  );
+    app.enableCors({
+        origin: [
+            'http://localhost:5173',
+            'http://localhost:3000',
+        ],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        allowedHeaders: [
+            'Content-Type',
+            'Authorization',
+            'x-refresh-token',
+            'x-access-token',
+        ],
+        exposedHeaders: [
+            'x-access-token',
+            'x-refresh-token',
+        ],
+    });
 
-  await app.listen(process.env.PORT ?? 3000);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true,
+            forbidNonWhitelisted: true,
+        }),
+    );
+
+    await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
